4d0781022e10294e77a5e2bb5493d61faf4a83c2
sprokkel78 <sprokkel78.bart@gmail.com>
2025-09-21 01:59:41 +0200

    use only 1 process

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ mkdir -p /usr/share/gwhatsapp-sprokkel78
 cp -r ./* /usr/share/gwhatsapp-sprokkel78/
 echo "#!/bin/sh" > /usr/bin/gwhatsapp
 echo "cd /usr/share/gwhatsapp-sprokkel78" >> /usr/bin/gwhatsapp
-echo "python3 ./gwhatsapp.py" >> /usr/bin/gwhatsapp
+echo "WEBKIT_USE_SINGLE_WEB_PROCESS=1 python3 ./gwhatsapp.py" >> /usr/bin/gwhatsapp
 cp ./gwhatsapp.desktop /usr/share/applications/com.sprokkel78.gwhatsapp.desktop
 chmod 755 /usr/bin/gwhatsapp
 chmod 664 /usr/share/gwhatsapp-sprokkel78/*
